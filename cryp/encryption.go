12cd4898254dadb1dd455cc4b2baec64a9988d61
Verkury <aleksandrlaty78@gmail.com>
2025-11-21 23:02:59 +0700

    Created first version of lib

---
+++ cryp/encryption.go
@@ -0,0 +1,37 @@
+package cryp
+
+import "fmt"
+
+var configDefault = make(map[string][]rune)
+var symbolsList string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя!% &*()+-="
+
+
+func MakeMap() map[string][]rune {
+	runes := []rune(symbolsList)
+	for n, v := range runes {
+		configDefault[decimalToBinary(n)] = []rune{v}
+	}
+	return configDefault
+}
+
+func decimalToBinary(n int) string {
+	if n == 0 {
+		return "0000000"
+	}
+
+	var binary string
+	for n > 0 {
+		remainder := n % 2
+		binary = fmt.Sprintf("%d%s", remainder, binary)
+		n = n / 2
+	}
+	for{
+		if (len(binary) < 7) {
+			binary = fmt.Sprintf("%s%s", "0", binary)
+		}
+		if (len(binary) >= 7) {
+			break
+		}
+	}
+	return binary
+}
