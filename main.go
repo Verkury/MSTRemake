291ad4f7b0679610c2d33056903204d473bf938f
Verkury <aleksandrlaty78@gmail.com>
2025-11-21 23:23:14 +0700

    Testing seed logik

--- main.go
+++ main.go
@@ -7,18 +7,16 @@ import (
 )
 
 func main() {
+	fmt.Println("Default")
 	cfg := cryp.MakeMap()
-	i := 0
-	num := 0
-	num1 := 0
 	for n, v := range cfg {
 		fmt.Println(n, "-", string(v))
-		i++
-		if (len(n) == 8) {
-			num++
-		} else {
-			num1++
-		}
 	}
-	fmt.Println(i, num, num1, num1+num)
+
+	fmt.Println("On seed 234234")
+	cfg1 := cryp.MakeMapS(234234)
+	for n, v := range cfg1 {
+		fmt.Println(n, "-", string(v))
+	}
+
 }
