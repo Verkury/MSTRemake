498066e5be356d045907fb93129877d621351384
Verkury <aleksandrlaty78@gmail.com>
2025-11-21 23:01:52 +0700

    Testing a cryp lib

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
-	_ "fmt"
+	"fmt"
+
+	"github.com/Verkury/MSTRemake/cryp"
 )
 
 func main() {
-	
+	cfg := cryp.MakeMap()
+	i := 0
+	num := 0
+	num1 := 0
+	for n, v := range cfg {
+		fmt.Println(n, "-", string(v))
+		i++
+		if (len(n) == 8) {
+			num++
+		} else {
+			num1++
+		}
+	}
+	fmt.Println(i, num, num1, num1+num)
 }
