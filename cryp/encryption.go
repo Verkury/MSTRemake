6e8e84928bc68c4f6334e9341359ab7cae4a22a0
Verkury <aleksandrlaty78@gmail.com>
2025-11-22 13:57:05 +0700

    Add more symbols and improved encryption algorithm

--- cryp/encryption.go
+++ cryp/encryption.go
@@ -5,63 +5,60 @@ import (
 )
 
 type MP struct{
-	mapIn map[string][]rune
+	mapIn map[string]rune
 	mapFrom map[string]string
 }
 
-var symbolsList string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя!,. *()+-="
+var symbolsList string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" +
+    "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя" +
+    "0123456789" + // Цифры
+    "!@#$%^&*()_+-=[]{}|;:,.<>?/~`\"'\\ " +
+	"¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾¿" +
+	"ҐЂЃЄЅІЇЈЉЊЋЌЎЏґђѓєѕіїјљњћќўџ" +
+    "─│┌┐└┘├┤┬┴┼▀▄█▌▐░▒▓⌠■∙√≈≤≥" + 
+	"ñòóôõö÷øùúû"
 
 func MakeMap() MP {
-	configDefaultIn := make(map[string][]rune)
+	configDefaultIn := make(map[string]rune)
 	configDefaultFrom := make(map[string]string)
 	runes := []rune(symbolsList)
 
 	for n, v := range runes {
 		binaryKey := decimalToBinary(n)
-		configDefaultIn[binaryKey] = []rune{v}
+		configDefaultIn[binaryKey] = v
 		configDefaultFrom[string(v)] = binaryKey
 	}
 	return MP{configDefaultIn, configDefaultFrom}
 }
 
 func MakeMapS(seed int) MP {
-	configDefaultIn := make(map[string][]rune)
+	configDefaultIn := make(map[string]rune)
 	configDefaultFrom := make(map[string]string)
-	shift := seed % 128
+	shift := seed % 256
 	runes := []rune(symbolsList)
 
 	for n, v := range runes {
-		newPos := (n+ shift) % 128
+		newPos := (n+ shift) % 256
 		binaryKey := decimalToBinary(newPos)
-		configDefaultIn[binaryKey] = []rune{v}
+		configDefaultIn[binaryKey] = v
 		configDefaultFrom[string(v)] = binaryKey
 	}
 	return MP{configDefaultIn, configDefaultFrom}
 }
 
 func decimalToBinary(n int) string {
-	if n == 0 {
-		return "0000000"
-	}
-
-	var binary string
-	for n > 0 {
-		remainder := n % 2
-		binary = fmt.Sprintf("%d%s", remainder, binary)
-		n = n / 2
-	}
-	for{
-		if (len(binary) < 7) {
-			binary = fmt.Sprintf("%s%s", "0", binary)
-		}
-		if (len(binary) >= 7) {
-			break
+	binary := ""
+	for i := 7; i >= 0; i-- {
+		if n&(1<<i) != 0 {
+			binary += "1"
+		} else {
+			binary += "0"
 		}
 	}
 	return binary
 }
 
-func Encrypt(mp MP, message string) string{
+func crypt(mp MP, message string) string{
 	var result string
 	
 	// Convert message to binary
@@ -76,20 +73,20 @@ func Encrypt(mp MP, message string) string{
 	
 	// Reverse binary string
 	var reversedBinary string
-	for _, r := range binaryString {
-		reversedBinary = string(r) + reversedBinary
+	for i := len(binaryString) - 1; i >= 0; i-- {
+		reversedBinary += string(binaryString[i])
 	}
 	
 	// Convert reversed binary back to characters (group by 8 bits)
-	for i := 0; i < len(reversedBinary); i += 7 {
-		end := i + 7
+	for i := 0; i < len(reversedBinary); i += 8 {
+		end := i + 8
 		if end > len(reversedBinary) {
 			end = len(reversedBinary)
 		}
 		binaryKey := reversedBinary[i:end]
 		
 		// Pad with zeros if needed
-		for len(binaryKey) < 7 {
+		for len(binaryKey) < 8 {
 			binaryKey += "0"
 		}
 		
@@ -108,3 +105,59 @@ func Encrypt(mp MP, message string) string{
 	return result
 }
 
+func Encrypt(mp MP, message string) string {
+    runes := []rune(message)
+    if len(runes) == 0 {
+        return crypt(mp, message)
+    }
+    
+    last := runes[len(runes)-1]
+    for i := len(runes) - 1; i > 0; i-- {
+        runes[i] = runes[i-1]
+    }
+    runes[0] = last
+    
+    return crypt(mp, string(runes))
+}
+
+func Decrypt(mp MP, encrypted string) string {
+    decrypted := crypt(mp, encrypted)
+    
+    runes := []rune(decrypted)
+    if len(runes) == 0 {
+        return decrypted
+    }
+    
+    first := runes[0]
+    for i := 0; i < len(runes)-1; i++ {
+        runes[i] = runes[i+1]
+    }
+    runes[len(runes)-1] = first
+    
+    return string(runes)
+}
+
+func Check() {
+	localmp := MakeMap()
+	fmt.Println("Testing...")
+	
+	runes := []rune(symbolsList)
+	test1 := len(runes) == 256
+	
+	testMessage := "Hello, World!"
+	encrypted := Encrypt(localmp, testMessage)
+	decrypted := Decrypt(localmp, encrypted)
+	test2 := testMessage == decrypted
+	
+	fmt.Println("Length test:", test1, "(", len(runes), "symbols )")
+	fmt.Println("Crypto test:", test2)
+	fmt.Println("Original:", testMessage)
+	fmt.Println("Encrypted:", encrypted)
+	fmt.Println("Decrypted:", decrypted)
+
+	if test1 && test2 {
+		fmt.Println("All tests passed!")
+	} else {
+		fmt.Println("Some tests failed!")
+	}
+}
