0996ef525b176e0f3ab51729230e82b53e0c0289
Verkury <aleksandrlaty78@gmail.com>
2025-11-22 12:32:22 +0700

    Testing

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
-	_ "fmt"
-	_ "github.com/Verkury/MSTRemake/cryp"
+	"fmt"
+	"github.com/Verkury/MSTRemake/cryp"
 )
 
 func main() {
-	
+	mp := cryp.MakeMapS(234152)
+	mp1 := cryp.MakeMap()
+	message := "Hello, world!"
+	message1 := "Привет, мир!"
+
+	fmt.Println(message, "->", cryp.Encrypt(mp, message), "->", cryp.Encrypt(mp, cryp.Encrypt(mp, message)))
+	fmt.Println(message1, "->", cryp.Encrypt(mp, message1), "->", cryp.Encrypt(mp, cryp.Encrypt(mp, message1)))
+	fmt.Println(message, "->", cryp.Encrypt(mp1, message), "->", cryp.Encrypt(mp1, cryp.Encrypt(mp1, message)))
+	fmt.Println(message1, "->", cryp.Encrypt(mp1, message1), "->", cryp.Encrypt(mp1, cryp.Encrypt(mp1, message1)))
+	fmt.Println("А теперь попытка расшифровать что-то с некоректным сидом")
+	fmt.Println(message, "->", cryp.Encrypt(mp, message), "->", cryp.Encrypt(mp, cryp.Encrypt(mp1, message)))
+	fmt.Println("Брух сиды можно стакать. Мощьность шифрования в абосолюте = 128^128")
 }
