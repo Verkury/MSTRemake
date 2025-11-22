1c6550976a78e5a5f958cabc051f8fe8259624c0
Verkury <aleksandrlaty78@gmail.com>
2025-11-22 11:19:25 +0700

    Temporarily cleared

--- main.go
+++ main.go
@@ -1,22 +1,10 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/Verkury/MSTRemake/cryp"
+	_ "fmt"
+	_ "github.com/Verkury/MSTRemake/cryp"
 )
 
 func main() {
-	fmt.Println("Default")
-	cfg := cryp.MakeMap()
-	for n, v := range cfg {
-		fmt.Println(n, "-", string(v))
-	}
-
-	fmt.Println("On seed 234234")
-	cfg1 := cryp.MakeMapS(234234)
-	for n, v := range cfg1 {
-		fmt.Println(n, "-", string(v))
-	}
-
+	
 }
